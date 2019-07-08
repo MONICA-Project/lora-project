4bb82ded782eaa8ae7e9fb2d798cb96a4c2a723c
BlubbFish <git@blubbfish.net>
2019-07-08 18:45:19 +0200

    Update Lora and Lora-Bot change update.sh so that it init the repositorys

--- update.sh
+++ update.sh
@@ -1,50 +1,166 @@
-echo "Switch to subdir Librarys"
-cd Librarys
-echo "Pull litjson"
-cd litjson
-git pull
-cd ..
-echo "Pull Mono.Posix"
-cd Mono.Posix
-git pull
-cd ..
-echo "Pull mqtt"
-cd mqtt
-git pull
-cd ..
-echo "Pull RaspberryIO"
-cd RaspberryIO
-git pull
-cd ..
-cd ..
+#!/bin/bash
+echo "Pull Librarys/litjson"
+if [ -a Librarys/litjson/.git ] 
+then
+	cd Librarys/litjson
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Librarys/litjson
+	cd Librarys/litjson
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Librarys/Mono.Posix"
+if [ -a Librarys/Mono.Posix/.git ] 
+then
+	cd Librarys/Mono.Posix
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Librarys/Mono.Posix
+	cd Librarys/Mono.Posix
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Librarys/mqtt"
+if [ -a Librarys/mqtt/.git ] 
+then
+	cd Librarys/mqtt
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Librarys/mqtt
+	cd Librarys/mqtt
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Librarys/RaspberryIO"
+if [ -a Librarys/RaspberryIO/.git ] 
+then
+	cd Librarys/RaspberryIO
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Librarys/RaspberryIO
+	cd Librarys/RaspberryIO
+	git checkout master
+	cd ..
+	cd ..
+fi
+
 echo "Pull Lora"
-cd Lora
-git pull
-cd ..
+if [ -a Lora/.git ] 
+then
+	cd Lora
+	git pull
+	cd ..
+else
+	git submodule update --init --  Lora
+	cd Lora
+	git checkout master
+	cd ..
+fi
+
 echo "Pull Lora-Bot"
-cd Lora-Bot
-git pull
-cd ..
-echo "Switch to subdir Utils"
-cd Utils
-echo "Pull Bot-Utils"
-cd Bot-Utils
-git pull
-cd ..
-echo "Pull ConnectorDataMqtt"
-cd ConnectorDataMqtt
-git pull
-cd ..
-echo "Pull Iot-Interfaces"
-cd Iot-Interfaces
-git pull
-cd ..
-echo "Pull Utils"
-cd Utils
-git pull
-cd ..
-echo "Pull Utils-IoT"
-cd Utils-IoT
-git pull
-cd ..
-read -p "Press return"
+if [ -a Lora-Bot/.git ] 
+then
+	cd Lora-Bot
+	git pull
+	cd ..
+else
+	git submodule update --init --  Lora-Bot
+	cd Lora-Bot
+	git checkout master
+	cd ..
+fi
+
+echo "Pull Utils/Bot-Utils"
+if [ -a Utils/Bot-Utils/.git ] 
+then
+	cd Utils/Bot-Utils
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Utils/Bot-Utils
+	cd Utils/Bot-Utils
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Utils/ConnectorDataMqtt"
+if [ -a Utils/ConnectorDataMqtt/.git ] 
+then
+	cd Utils/ConnectorDataMqtt
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Utils/ConnectorDataMqtt
+	cd Utils/ConnectorDataMqtt
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Utils/Iot-Interfaces"
+if [ -a Utils/Iot-Interfaces/.git ] 
+then
+	cd Utils/Iot-Interfaces
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Utils/Iot-Interfaces
+	cd Utils/Iot-Interfaces
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Utils/Utils"
+if [ -a Utils/Utils/.git ] 
+then
+	cd Utils/Utils
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Utils/Utils
+	cd Utils/Utils
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Utils/Utils-IoT"
+if [ -a Utils/Utils-IoT/.git ] 
+then
+	cd Utils/Utils-IoT
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Utils/Utils-IoT
+	cd Utils/Utils-IoT
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+if [ -z "$1" ]
+then
+	read -p "Please Press key"
+fi
